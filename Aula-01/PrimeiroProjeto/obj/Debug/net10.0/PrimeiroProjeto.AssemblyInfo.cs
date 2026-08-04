7cbca7a752de553b8e61fa6aa7897b91d247b33f
Creator-Gm <venom2guil.cm@gmail.com>
2026-08-04 11:25:16 -0300

    Revisao de fundamentos de c# parte 1

--- Aula-01/PrimeiroProjeto/obj/Debug/net10.0/PrimeiroProjeto.AssemblyInfo.cs
+++ Aula-01/PrimeiroProjeto/obj/Debug/net10.0/PrimeiroProjeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16949d08127a118b2600c03898cb2bdb513468c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50d6af5a984cd878137ee51a514f9148302434f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
