222bf942eb7b2ffecea336d50e5c0849c00e584b
Creator-Gm <venom2guil.cm@gmail.com>
2026-08-04 14:59:23 -0300

    Revisão dos vetores

--- Aula-03/Repeticao/Program.cs
+++ Aula-03/Repeticao/Program.cs
@@ -1,12 +1,49 @@
-﻿internal class Program
+﻿using System.ComponentModel;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
+    /*
+    sintaxe:
+    enquanto uma condição for verdadeira
+            execute um conjunto de comandos
+        for --> quando sabemos a quantidede de repetições
+        while -> quando a repetição depende de uma condição 
+        */
+        //laço for: imprimir números de 1 ate 18
+        Console.WriteLine("imprimir contagem regressiva ");
+        for (int numero = 10; numero <= 0; numero--)
+        {
+       Console.WriteLine(numero);
+        }
+        //-----------------------
+        int contador = 1;
+        while (contador <= 10)
+        {
+                Console.WriteLine(contador);
+                contador++;
+        }
+        //laço while: solicitar a senha e acertar 
+        string senha = "";
+        while(senha != "1234")
+        {
+            Console.Write("digite a senha: ");
+            senha = Console.ReadLine();
+        }
+        Console.WriteLine("Acesso permitido!");
+        //---------------------------------------
+       int num;
+        do
+        {
+            Console.Write("Digite um numero positivo");
+            num = int.Parse(Console.ReadLine());
+
+        } while (num <= 0);
 
 
 
 
-        
-        Console.WriteLine("Hello, World!");
+
     }
 }
---
+++ Aula-03/Vetores/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+     /* vetores 4e matrizes
+     vetor => e uma estrutura de dados ultilizando para armazenar 
+     varios valores de um mes o tipo dentro de uam unica variavel 
+    Sintaxe: tipo [] nome_vetor = new tipo[tamanho];
+    
+    iniciar um vetor -->lista de frutas 
+    */
+    string[] listafrutas = {"Maça","manga","morango","melancia","melão"};
+        Console.WriteLine("acessar o terceiro elemento de lista: ");
+        Console.WriteLine(listafrutas[2]);
+               Console.WriteLine("alterar o segundo elemnto da lista: ");
+        listafrutas[1] = "Banana";
+        foreach (string fruta in listafrutas)
+        {
+                     Console.WriteLine(fruta); 
+        }
+
+
+
+
+    }
+}
