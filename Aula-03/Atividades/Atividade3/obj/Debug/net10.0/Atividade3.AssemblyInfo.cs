313361e09ce1f0e05cf42650f2dc735605b21c08
Creator-Gm <venom2guil.cm@gmail.com>
2026-08-12 08:33:39 -0300

    atividade resolvida

--- Aula-03/Atividades/Atividade3/obj/Debug/net10.0/Atividade3.AssemblyInfo.cs
+++ Aula-03/Atividades/Atividade3/obj/Debug/net10.0/Atividade3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Atividade3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03cb0e9f1dceb7c4c50f3d1599199cf06bb9142f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0523825d39df4befdf6c9523f04057650825f143")]
 [assembly: System.Reflection.AssemblyProductAttribute("Atividade3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Atividade3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
